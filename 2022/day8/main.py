fe122680c946ccd19a13345bfcaf2af5bdcb4d70
vagrant <vagrant@kali>
2022-12-11 12:12:05 -0500

    Add images for day8

--- 2022/day8/main.py
+++ 2022/day8/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 from enum import Enum
+from PIL import Image
+from itertools import chain
 
 def read_file(filename):
     with open(filename) as f:
@@ -98,4 +100,65 @@ def part2():
 
 
 part1()
-#part2()
+part2()
+
+def get_scenic_score(trees):
+    scenic_scores = [[0 for tree in tree_row] for tree_row in trees]
+
+    forest_edge_length = len(trees)
+
+    for tree_x in range(0, forest_edge_length):
+        for tree_y in range(0, forest_edge_length):
+            tree_size = trees[tree_x][tree_y]
+            scenic_score = 1
+
+            for direction in Direction:
+                x = tree_x
+                y = tree_y
+                direction_scenic_score = 0
+                
+                while True:
+                    x += direction.value[0]
+                    y += direction.value[1]
+                    if x < 0 or y < 0 or x >= forest_edge_length or y >= forest_edge_length:
+                        # We are out of the forest
+                        break
+                    else:
+                        scenic_tree_size = trees[x][y]
+                        direction_scenic_score += 1
+                        if scenic_tree_size >= tree_size:
+                            # Tree is to high, we cannot see any more tree
+                            break
+                        else:
+                            # Tree is below ours, we can see more trees
+                            pass
+
+                scenic_score *= max(1, direction_scenic_score)
+
+            scenic_scores[tree_x][tree_y] = scenic_score
+    return scenic_scores
+
+def get_green_shade(green_value: int):
+    rgb_red = 0
+    rgb_green = green_value
+    rgb_blue = 0
+    return 256 * 256 * rgb_blue + 256 * rgb_green + rgb_red
+
+def generate_forest_png():
+    forest_aoa = [[int(tree) for tree in tree_row] for tree_row in read_file('input.txt')]
+    forest = [int(tree) for tree_row in forest_aoa for tree in tree_row]
+    
+    image = Image.new('RGB', (len(forest_aoa), len(forest_aoa[0])))
+    pixels = [get_green_shade(25 * tree) for tree in forest]
+    image.putdata(pixels)
+    image.save('forest.png')
+
+    scenic_scores = [score for score_row in get_scenic_score(forest_aoa) for score in score_row]
+    max_scenic_scores = max(scenic_scores)
+    r = max_scenic_scores / 255
+    image2 = Image.new('RGB', (len(forest_aoa), len(forest_aoa[0])))
+    pixels2 = [get_green_shade(int(score // r)) for score in scenic_scores]
+    image2.putdata(pixels2)
+    image2.save('scenic_score.png')
+
+generate_forest_png()
