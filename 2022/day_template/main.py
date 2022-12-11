e014c1dca2e7d43ab6e3783756279d9f18fbe6ed
vagrant <vagrant@kali>
2022-12-11 14:03:25 -0500

    Add template

---
+++ 2022/day_template/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def read_file(filename):
+    with open(filename) as f:
+        return [row.strip('\n') for row in f.readlines()]
+
+def part1():
+    pass
+
+def part2():
+    pass
+
+part1()
+part2()
